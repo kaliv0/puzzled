e66d18ec2a4937bd4e3b1c2f24beab1e3a2a66af
kaliv0 <kaloyan.ivanov88@gmail.com>
2023-11-01 20:59:05 +0200

    added main.py

---
+++ leet_rank/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+# from database import models
+from db.database import engine
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# app.include_router(post.router)
+
+models.Base.metadata.create_all(engine)
+
+origins = [
+  'http://localhost:3000',
+  'http://localhost:3001'
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=['*'],
+  allow_headers=['*']
+)
