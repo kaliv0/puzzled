545b0fd5e53f06f4637916940024bf8c90892e64
kaliv0 <kaloyan.ivanov88@gmail.com>
2023-11-03 01:21:20 +0200

    added solution model

--- leet_rank/alembic/versions/5e58aba07143_added_testdata.py
+++ leet_rank/alembic/versions/5e58aba07143_added_testdata.py
@@ -7,9 +7,9 @@ Create Date: 2023-11-02 17:19:47.103379
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '5e58aba07143'
--- leet_rank/db/models.py
+++ leet_rank/db/models.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey
-from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy.sql.sqltypes import DateTime, Integer, String
 
 from db.database import Base
 
@@ -18,27 +21,55 @@ class Task(Base):
     test_data = relationship(
         "TaskDescription", back_populates="task", cascade="all, delete"
     )
-    # author's solution (optional)
+    author_solution_id = Column(Integer, ForeignKey("solutions.id"), nullable=False)
+    author_solution = relationship(
+        "Solution", back_populates="task", cascade="all, delete"
+    )
+    user_solutions = relationship(
+        "Solution", back_populates="task", cascade="all, delete"
+    )
+    # author_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    # author = relationship("User", back_populates="tasks")
+
     # category (list?)
     # tags
     # tips
     # difficulty level
-    # solutions
-    # author
     # votes
 
 
-# class Image(Base):
-#     __tabelname__ = "images"
+class Solution(Base):
+    __tablename__ = "solutions"
 
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     content: Mapped[bytearray] = mapped_column()
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    task_id = Column(Integer, ForeignKey("tasks.id"), nullable=False)
+    task = relationship("Task", back_populates="solutions", cascade="all, delete")
+    # author_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    # author = relationship("User", back_populates="solutions")
+    description_id = Column(Integer, ForeignKey("descriptions.id"), nullable=False)
+    description = relationship(
+        "Description", back_populates="solutions", cascade="all, delete"
+    )
+    create_date = Column(DateTime, default=datetime.now())  # TODO: verify
+    last_modified = (Column(DateTime, onupdate=datetime.now()),)
+    content = Column(String, nullable=False)
+    # language
+    # votes
+
+
+class Image(Base):
+    __tablename__ = "images"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    # content = Column(File??)
+    upload_date = Column(DateTime, default=datetime.now())  # TODO: verify
+    # author_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    # author = relationship("User", back_populates="solutions")
 
 
 class DescriptionMixin(object):
     text = Column(String)
-    # images: Mapped[List[Image]] = mapped_column()
-    # links: Mapped[List[str]] = mapped_column()
+    links = Column(ARRAY(String))
 
 
 class TaskDescription(DescriptionMixin, Base):
@@ -48,13 +79,19 @@ class TaskDescription(DescriptionMixin, Base):
     task_id = Column(Integer, ForeignKey("tasks.id"), nullable=False)
     task = relationship("Task", back_populates="description")
     # task = relationship("Task", back_populates="description", backref=backref("items", cascade="all, delete-orphan"))
+    images = relationship(
+        "Image", back_populates="task_description", cascade="all, delete"
+    )
 
 
-# class SolutionDescription(DescriptionMixin, Base):
-#     __tablename__ = "solution_descriptions"
+class SolutionDescription(DescriptionMixin, Base):
+    __tablename__ = "solution_descriptions"
 
-#     solution_id = Column(Integer, ForeignKey("solutions.id"))
-#     solution = relationship("Task", back_populates="description")
+    solution_id = Column(Integer, ForeignKey("solutions.id"))
+    solution = relationship("Solution", back_populates="description")
+    images = relationship(
+        "Image", back_populates="solution_description", cascade="all, delete"
+    )
 
 
 class TestCase(Base):
