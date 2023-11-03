a2d7cf974bb9bfcf81cbff965b432d6eb835a3f3
kaliv0 <kaloyan.ivanov88@gmail.com>
2023-11-04 00:18:48 +0200

    added .env file

--- leet_rank/db/database.py
+++ leet_rank/db/database.py
@@ -1,13 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://kaliv0:pass123@localhost:5432/leetdb"
+# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://kaliv0:pass123@localhost:5432/leetdb"
+
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base = declarative_base()
 
 
 def get_db():
